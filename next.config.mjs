a4b9a8ecaefd5e7fbce47c29962a6473f4ea7155
KlaudiuszB5528 <klaudiusz.biegacz@gmail.com>
2024-04-25 19:54:57 +0200

    next-intl config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+const createNextIntlPlugin = require("next-intl/plugin");
+
+const withNextIntl = createNextIntlPlugin("./i18n/i18n.ts");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+module.exports = withNextIntl(nextConfig);
